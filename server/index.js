149174aac8d02095eb1a12503cf2d49f4876941a
seethapa <seethapaul@gmail.com>
2025-09-12 13:53:00 +0530

    client origin link changed

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'http://localhost:3001', // allow your frontend origin
+  origin: 'user-register-mern-5p25.vercel.app', // allow your frontend origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
