28ed8fda8bbb436a8de7e8f2d6c71bda267717bd
seethapa <seethapaul@gmail.com>
2025-09-12 13:44:59 +0530

    server link changed

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext } from 'react';
 
 export const AppContext = createContext();
 export const AppProvider = ({ children }) => {
-  const backendUrl = 'http://localhost:4001/api';
+  const backendUrl = 'https://user-register-mern.vercel.app/api';
 // const backendUrl = BACKEND_URL;
   return (
     <AppContext.Provider value={{ backendUrl }}>
