0579ebf6a58a71af37ec03f5d87b36f996eb6057
seethapa <seethapaul@gmail.com>
2025-09-14 17:26:01 +0530

    origin url changed

--- server/index.js
+++ server/index.js
@@ -23,7 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'https://samuelasirvatham.com' ; //  'https://user-register-mern-5p25.vercel.app', // allow your frontend origin
+  origin: 'https://samuelasirvatham.com' ,
+  //  'https://user-register-mern-5p25.vercel.app', // allow your frontend origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
